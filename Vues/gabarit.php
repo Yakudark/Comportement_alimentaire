714d047229c70c9986a32e6c549357c883708a32
Yradouani <yasmine.radouani@outlook.fr>
2023-02-23 10:50:53 +0100

    ajustement des modèles + ajout d'éléments de sécurité

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -1,3 +1,12 @@
+<?php session_start();
+
+// Vérification de l'authentification de l'utilisateur
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    header('Location: index.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +17,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" src="index.js"></script>
+    <link rel="stylesheet" href="./Style/style.css">
     <title><?= $titre ?></title>
 </head>
 
